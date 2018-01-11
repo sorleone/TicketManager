3d8e45d461a400e9ebb2f39edcb3615a5f2f9df5
sorleone <30836325+sorleone@users.noreply.github.com>
2018-01-11 11:32:54 +0100

    ROSSO: customerStrategyTest

--- src/test/java/it/uniupo/disit/se/lab10/view/TestView.java
+++ src/test/java/it/uniupo/disit/se/lab10/view/TestView.java
@@ -3,13 +3,30 @@ package it.uniupo.disit.se.lab10.view;
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
+
+import it.uniupo.disit.se.lab10.model.Model;
+import it.uniupo.disit.se.lab10.model.TicketManager;
 
 public class TestView {
 
-	@Test@Ignore
-	public void test() {
-		fail("Not yet implemented");
+    private Model ticketManager;
+    
+    @Before
+    public void setUp() {
+        ticketManager = new TicketManager();
+    }
+    
+	@Test
+	public void customerStrategyTest() {
+        RoleStrategy customerRole = new CustomerStrategy();
+        assertEquals(0, customerRole.getTicketNumber(ticketManager));
+        customerRole.incTicketNumber(ticketManager);
+        assertEquals(1, customerRole.getTicketNumber(ticketManager));
 	}
 }
+
+
+
+
