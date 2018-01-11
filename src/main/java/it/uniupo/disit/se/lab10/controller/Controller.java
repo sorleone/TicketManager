3733a10ad039e4ea9711e275691b80984cb5a722
sorleone <30836325+sorleone@users.noreply.github.com>
2018-01-11 13:16:46 +0100

    VERDE: controllerTest

--- src/main/java/it/uniupo/disit/se/lab10/controller/Controller.java
+++ src/main/java/it/uniupo/disit/se/lab10/controller/Controller.java
@@ -7,13 +7,18 @@ import it.uniupo.disit.se.lab10.view.TicketManagerView;
 
 public class Controller extends AbstractController {
 
+    private Model model;
+    private TicketManagerView view;
     
     public Controller(TicketManagerView view, Model model) {
-
+        this.view = view;
+        this.model = model;
+        this.view.addListener(this);
+        this.model.addObserver(this.view);
     }
 
     @Override
     public void actionPerformed(ActionEvent arg0) {
-
+        view.getRole().incTicketNumber(model);
     }
 }
