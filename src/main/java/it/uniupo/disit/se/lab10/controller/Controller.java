e761019f38a150feb89b86aa6023120d20d6c75d
sorleone <30836325+sorleone@users.noreply.github.com>
2018-01-11 13:16:26 +0100

    ROSSO: controllerTest

--- src/main/java/it/uniupo/disit/se/lab10/controller/Controller.java
+++ src/main/java/it/uniupo/disit/se/lab10/controller/Controller.java
@@ -1,11 +1,19 @@
 package it.uniupo.disit.se.lab10.controller;
 
+import java.awt.event.ActionEvent;
+
 import it.uniupo.disit.se.lab10.model.Model;
 import it.uniupo.disit.se.lab10.view.TicketManagerView;
 
-public class Controller {
+public class Controller extends AbstractController {
 
-    public Controller(TicketManagerView clientUI1, Model ticketManager) {
-        // TODO Auto-generated constructor stub
+    
+    public Controller(TicketManagerView view, Model model) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+
     }
 }
--- src/test/java/it/uniupo/disit/se/lab10/controller/TestController.java
+++ src/test/java/it/uniupo/disit/se/lab10/controller/TestController.java
@@ -4,13 +4,23 @@ import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.junit.Ignore;
+
+import it.uniupo.disit.se.lab10.model.Model;
+import it.uniupo.disit.se.lab10.model.TicketManager;
+import it.uniupo.disit.se.lab10.view.CustomerStrategy;
+import it.uniupo.disit.se.lab10.view.TicketManagerUI;
+
 
 public class TestController {
 
-	@Test@Ignore
-	public void test() {
-		fail("Not yet implemented");
+	@Test
+	public void controllerTest() {
+	    CustomerStrategy customerStrategy = new CustomerStrategy();
+	    TicketManagerUI clientUI1 = new TicketManagerUI("client1", customerStrategy);
+	    Model ticketManager = new TicketManager();
+	    Controller controller = new Controller(clientUI1, ticketManager);
+	    assertEquals(0, customerStrategy.getTicketNumber(ticketManager));
+	    controller.actionPerformed(null);
+	    assertEquals(1, customerStrategy.getTicketNumber(ticketManager));
 	}
-
 }
